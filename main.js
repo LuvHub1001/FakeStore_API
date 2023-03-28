683c9bfc04abffcaa7235887c40a8caef8f3cbfb
dy7791 <dy779@jei.com>
2023-03-28 13:52:25 +0900

    add: categoryEvent

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-let url;
 const productList = [];
+const categoryList = [];
 const content = document.getElementById("content");
+const categoryArea = document.getElementById("categoryArea");
 
 const productAPI = async () => {
-  url = new URL("https://fakestoreapi.com/products");
+  const url = new URL("https://fakestoreapi.com/products");
   const res = await fetch(url);
   const data = await res.json();
   productList.push(...data);
   productRender();
 };
 productAPI();
+
+const categoryAPI = async () => {
+  const url = "https://fakestoreapi.com/products/categories";
+  const res = await fetch(url);
+  const categoryData = await res.json();
+  categoryList.push(...categoryData);
+  categoryRender();
+};
+categoryAPI();
 
 const productRender = () => {
   productList.forEach((items) => {
@@ -24,7 +34,27 @@ const productRender = () => {
   });
 };
 
-const apis = () => {
-  baseURL = "https://fakestoreapi.com";
-  return {};
+const categoryRender = () => {
+  categoryHTML = "";
+  categoryList.forEach((items, idx) => {
+    categoryHTML += `<div id=${idx}>${items}</div>`;
+  });
+  categoryArea.innerHTML = categoryHTML;
 };
+
+categoryArea.addEventListener("click", (event) => {
+  moveToCategory(event);
+});
+
+const moveToCategory = async (event) => {
+  const categoryVal = event.target.textContent;
+  const url = new URL(
+    `https://fakestoreapi.com/products/category/${categoryVal}`
+  );
+  productAPI();
+};
+
+// const apis = () => {
+//     baseURL = "https://fakestoreapi.com";
+//     return {};
+//   };
